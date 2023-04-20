e14b041c10a9794214178cf6c290a06059fd52f2
satishk38 <satishk38@gmail.com>
2023-04-20 17:48:35 -0400

    Currency Conversion intial changes

---
+++ src/main/java/com/currency/convertor/CurrencyConverterController.java
@@ -0,0 +1,67 @@
+package com.currency.convertor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+@Controller
+
+public class CurrencyConverterController {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+    private final String exchangeRatesUrl = "https://openexchangerates.org/api/latest.json";
+    private final String apiKey;
+    private final Map<String, Double> exchangeRatesCache = new HashMap<>();
+    private Instant lastExchangeRatesUpdate;
+
+    public CurrencyConverterController(@Value("${openexchangerates.api.key}") String apiKey) {
+        this.apiKey = apiKey;
+    }
+
+    @GetMapping("/convert")
+    @ResponseBody
+    public double convertCurrency(@RequestParam("source") String sourceCurrency,
+                                  @RequestParam("target") String targetCurrency,
+                                  @RequestParam("amount") double amount) {
+        // Check if exchange rates cache is stale, and update if necessary
+        if (lastExchangeRatesUpdate == null || Duration.between(lastExchangeRatesUpdate, Instant.now()).toHours() >= 1) {
+            updateExchangeRatesCache();
+        }
+
+        // Check if exchange rate for the requested currency pair is in the cache
+        String currencyPair = sourceCurrency + "_" + targetCurrency;
+        Double exchangeRate = exchangeRatesCache.get(currencyPair);
+        if (exchangeRate == null) {
+            throw new IllegalArgumentException("Exchange rate not available for " + currencyPair);
+        }
+
+        // Calculate the converted amount and return it
+        double convertedAmount = amount * exchangeRate;
+        System.out.println("**********   "+convertedAmount);
+        return convertedAmount;
+    }
+
+    private void updateExchangeRatesCache() {
+        String url = exchangeRatesUrl + "?app_id=" + apiKey + "&base=USD";
+        ResponseEntity<ExchangeRatesResponse> response = restTemplate.getForEntity(url, ExchangeRatesResponse.class);
+        ExchangeRatesResponse exchangeRates = response.getBody();
+
+        exchangeRatesCache.clear();
+        exchangeRates.getRates().forEach((currencyCode, exchangeRate) -> {
+            String currencyPair = "USD_" + currencyCode;
+            exchangeRatesCache.put(currencyPair, exchangeRate);
+        });
+
+        lastExchangeRatesUpdate = Instant.now();
+    }
+
+}
---
+++ src/main/java/com/currency/convertor/ExchangeRatesResponse.java
@@ -0,0 +1,22 @@
+package com.currency.convertor;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Map;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ExchangeRatesResponse {
+
+    @JsonProperty("rates")
+    private Map<String, Double> rates;
+
+    public Map<String, Double> getRates() {
+        return rates;
+    }
+
+    public void setRates(Map<String, Double> rates) {
+        this.rates = rates;
+    }
+
+}
